141150d1b8255e86baf7a10ea18e7e0833a274ff
miradii <mrtuz@protonmail.com>
2020-12-03 15:27:01 +0330

    added buttons and completed functionality

--- script.js
+++ script.js
@@ -8,4 +8,33 @@ function createCells(row) {
     container.appendChild(div)
   }
 }
-createCells(5)
+createCells(16)
+
+/* customize row size*/
+const rowInput = document.getElementById("row-input")
+const getRowSize = () => Number(rowInput.value)
+function customRows() {
+  clear()
+  createCells(getRowSize())
+}
+document.querySelector("#apply-btn").addEventListener("click", customRows)
+
+/*coloring logic*/
+function addColorClass(event) {
+  event.target.classList.add("colored")
+}
+function enableColoring() {
+  document.querySelector(".pad").addEventListener("mouseover", addColorClass)
+}
+function disabelColoring() {
+  document.querySelector(".pad").removeEventListener("mouseover", addColorClass)
+}
+document.querySelector(".pad").addEventListener("mousedown", enableColoring)
+document.querySelector(".pad").addEventListener("mouseup", disabelColoring)
+/*clear*/
+function clear() {
+  const cells = document.querySelectorAll(".padcell")
+  cells.forEach((cell) => cell.classList.remove("colored"))
+}
+
+document.getElementById("clear-btn").addEventListener("click", clear)
